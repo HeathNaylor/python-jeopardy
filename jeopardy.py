8b4e1b40ca4396e6fefb4a39496ef225a1d10323
Heath Naylor <heath@bythepixel.com>
2019-05-21 01:19:29 -0600

    Wire up threading and websocket server

--- jeopardy.py
+++ jeopardy.py
@@ -6,10 +6,14 @@ from asciimatics.event import KeyboardEvent
 from asciimatics.screen import Screen
 from asciimatics.scene import Scene
 from CategoriesModel import categories
+import threading
+import asyncio
+import websockets
 import sys
 import os
 
 clue_position = {"row": 0, "column": 0}
+players = []
 
 class JeopardyFrame(Frame):
     def __init__(self, screen):
@@ -42,6 +46,8 @@ class JeopardyFrame(Frame):
         if isinstance(event, KeyboardEvent):
             if event.key_code in [ord('q'), ord('Q'), Screen.ctrl("c")]:
                 raise StopApplication("User quit")
+            if event.key_code in [ord('p'), ord('P')]:
+                raise NextScene("Players")
 
         # Now pass on to lower levels for normal handling of the event.
         return super(JeopardyFrame, self).process_event(event)
@@ -85,13 +91,20 @@ class ClueFrame(Frame):
         self.add_layout(layout2)
         self.add_effect(self._figlet(self._canvas, {"text": "CLUE", "font": 'starwars'},[screen.width // 2, 1], 20))
 
-        self.padding = Label("", align='^', height=10)
-        self.padding.custom_colour = "field"
-        layout.add_widget(self.padding, 0)
+        padding = Label("", align='^', height=10)
+        padding.custom_colour = "field"
+        layout.add_widget(padding, 0)
 
-        self.clue = Label(categories[clue_position['column']]['clues'][clue_position['row']]['clue'])
+        self.clue = Label("", align='^')
         self.clue.custom_colour = "field"
         layout2.add_widget(self.clue, 0)
+
+        question_padding = Label("", align='^', height=5)
+        layout2.add_widget(question_padding, 0)
+
+        self.question = Label("", align='^')
+        self.question.custom_colour = "field"
+        layout2.add_widget(self.question, 0)
 
         # Prepare the Frame for use.
         self.fix()
@@ -107,11 +120,11 @@ class ClueFrame(Frame):
         )
 
     def _toggle_answer(self):
-        if len(self.padding.text):
-            self.padding.text = ""
+        if len(self.question.text):
+            self.question.text = ""
             return
 
-        self.padding.text = "Partayyy!"
+        self.question.text = categories[clue_position['column']]['clues'][clue_position['row']]['question']
 
     def _loaded(self):
         self.clue.text = categories[clue_position['column']]['clues'][clue_position['row']]['clue']
@@ -123,20 +136,80 @@ class ClueFrame(Frame):
             if event.key_code == ord('r'):
                 self._toggle_answer()
             if event.key_code in [ord('q'), ord('Q'), Screen.ctrl("c")]:
+                self.clue.text = ""
+                self.question.text = ""
                 raise NextScene("Main")
 
         # Now pass on to lower levels for normal handling of the event.
         return super(ClueFrame, self).process_event(event)
 
+class PlayersFrame(Frame):
+    def __init__(self, screen):
+        super(PlayersFrame, self).__init__(
+            screen,
+            screen.height,
+            screen.width,
+            has_border=False,
+            hover_focus=True,
+            name="Players"
+        )
+        layout = Layout([1])
+        layout2 = Layout([1])
+        self.add_layout(layout)
+        self.add_layout(layout2)
+
+        padding = Label("", align='^', height=5)
+        padding.custom_colour = "field"
+        layout.add_widget(padding, 0)
+
+        self.players = Label("Test", height=10, align='^')
+        self.players.custom_colour = "field"
+        layout2.add_widget(self.players, 0)
+        self.fix()
+
+    def _loaded(self):
+        self.players.text = "\n".join(players)
+
+    def process_event(self, event):
+        # Do the key handling for this Frame.
+        if isinstance(event, KeyboardEvent):
+            if event.key_code in [ord('p'), ord('P')]:
+                raise NextScene("Players")
+            if event.key_code in [ord('c'), ord('C')]:
+                players.clear()
+                raise NextScene("Players")
+            if event.key_code in [ord('q'), ord('Q'), Screen.ctrl("c")]:
+                raise NextScene("Main")
 
 def jeopardy(screen, old_scene):
     clue_frame = ClueFrame(screen)
     clue_frame._on_load = lambda: clue_frame._loaded()
+
+    players_frame = PlayersFrame(screen)
+    players_frame._on_load = lambda: players_frame._loaded()
+
     scenes = [
         Scene([JeopardyFrame(screen)], name="Main"),
-        Scene([clue_frame], name="Clue", clear=True)
+        Scene([clue_frame], name="Clue", clear=True),
+        Scene([players_frame], name="Players"),
     ]
     screen.play(scenes, stop_on_resize=True, start_scene=old_scene, allow_int=True)
+
+def server():
+    asyncio.set_event_loop(asyncio.new_event_loop())
+    start_server = websockets.serve(hello, 'localhost', 8765)
+
+    asyncio.get_event_loop().run_until_complete(start_server)
+    asyncio.get_event_loop().run_forever()
+
+async def hello(websocket, path):
+    name = await websocket.recv()
+    players.append(name)
+    greeting = f"Hello {name}!"
+    await websocket.send(greeting)
+
+thread = threading.Thread(target=server)
+thread.start()
 
 last_scene = None
 while True:
